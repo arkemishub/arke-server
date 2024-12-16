b59edc21875830c42e166bbffe0c26fd8d308de1
vittorio-reinaudo <v.reinaudo@mokkastudios.com>
2024-12-16 14:30:02 +0100

    v0.3.15

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ArkeServer.MixProject do
   use Mix.Project
 
-  @version "0.3.14"
+  @version "0.3.15"
   @scm_url "https://github.com/arkemishub/arke-server"
   @site_url "https://arkehub.com"
 
