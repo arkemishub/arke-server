98207566300da061f2dda84027d2e99afb62f317
Erik <e.ferrari@arkemis.it>
2025-01-14 12:40:07 +0100

    chore: remove macro

--- lib/arke_server/views/error_view.ex
+++ lib/arke_server/views/error_view.ex
@@ -27,58 +27,7 @@ defmodule ArkeServer.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
-
-  defmacro __using__(_) do
-    quote do
-      require Logger
-
-      alias Arke.Errors.ArkeError
-      alias Arke.Utils.ErrorGenerator
-
-      def render(any_status, %{reason: %ArkeError{message: msg} = error} = assigns) do
-        log_error_message(assigns, ArkeError.message(error))
-        %{content: nil, message: msg}
-      end
-
-      def render(any_status, %{reason: %ArkeError{}} = assigns) do
-        {:error, err} = handle_error(assigns)
-        err
-      end
-
-
-      def handle_error(
-            %{
-              reason: %ArkeError{message: msg} = arke_error
-            } = assigns
-          ) do
-        log_error_message(assigns, arke_error)
-        msg
-      end
-
-      def handle_error(assigns) do
-        log_error_message(assigns)
-        ErrorGenerator.create(:generic, assigns.reason)
-      end
-
-      defp log_error_message(assigns, error_message \\ "") do
-        [{first_module_of_stack, _, _, _} | _] = assigns.stack
-        message = "running #{first_module_of_stack} terminated\n"
-        message = message <> error_message
-
-        message =
-          message <>
-            Enum.reduce(assigns.stack, "", fn {module, function, fun_param, info}, acc ->
-              #          file = Keyword.get(info, :file) #Used to take path of file in error
-              line = Keyword.get(info, :line)
-
-              "#{to_string(acc)}\t(#{to_string(module)}) #{to_string(function)}/#{to_string(fun_param)} line: #{to_string(line)}\n"
-
-              # OTHER WAY TO LOG WITH FILE SPECIFICATION
-              #          "#{to_string(acc)}\t(#{to_string(module)}) #{to_string(function)}/#{to_string(fun_param)} \n\t\t #{file}: line: #{to_string(line)}\n"
-            end)
-
-        Logger.error(message)
-      end
-    end
+  def template_not_found(template, _assigns) do
+    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 end
