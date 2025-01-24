1b49f030aa7cda6ad9c23e923063e61222e69b31
vittorio-reinaudo <v.reinaudo@mokkastudios.com>
2025-01-24 15:38:23 +0100

    chore: clean csp.ex

--- lib/arke_server/plugs/csp.ex
+++ lib/arke_server/plugs/csp.ex
@@ -8,8 +8,6 @@ defmodule ArkeServer.Plugs.ContentSecurityPolicy do
       "default-src 'self'; " <>
       "script-src 'self' cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-bundle.js cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.js 'unsafe-inline';" <>
       "style-src 'self' cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.css; 'unsafe-inline'" <>
-      # "img-src 'self' https://trusted-images.example.com; " <>
-      # "connect-src 'self' https://api.example.com; " <>
       "frame-src 'none'; " <>
       "base-uri 'self'; " <>
       "form-action 'self'"
