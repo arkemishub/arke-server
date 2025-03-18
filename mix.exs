c6e5141fb8c9f8932dd4b4d4299e2eeeb6802a88
vittorio-reinaudo <v.reinaudo@mokkastudios.com>
2025-03-18 09:56:39 +0100

    chore: update library

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule ArkeServer.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     List.flatten([
-      {:phoenix, "~> 1.6.6"},
+      {:phoenix, "~> 1.7"},
+      {:phoenix_view, "~> 2.0"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
